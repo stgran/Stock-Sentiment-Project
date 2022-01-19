615dc5ade56a7b2018da9e0573d4f6b37250a8e1
Soren Gran <sorentgran@gmail.com>
2022-01-19 10:43:58 -0500

    Main file for scraping specifically Palantir tweets. Acts as template for main files for scraping tweets regarding other stocks by changing the search term.

---
+++ scrp_pltr_twts.py
@@ -0,0 +1,9 @@
+from twitter_scraper_methods_class import TwitterScraper
+
+def main():
+    scraper = TwitterScraper(query_terms='palantir', db_table='palantir_tweets', use_since_id=False)
+    results = scraper.run()
+    results.to_csv('test_011022.csv')
+
+if __name__ == "__main__":
+    main()
